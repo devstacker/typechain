0bfa7d9707d4b1ba254d37b0c661e6dac0a9378e
devstacker <allstacks.io@gmail.com>
2019-03-14 18:49:31 +0900

    interface on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return (`Hello ${name}, you are ${age}, you are a ${gender}!`);
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "dahye",
+  age: 29,
+  gender: "female"
 };
 
-console.log(sayHi("dahye", 29, "female"));
+const sayHi = (person: Human): string => {
+  return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
+};
+
+console.log(sayHi(person));
 export {};
